897b2af00aaaaaf0cc538844e21c09047f36a45b
Your Name <you@example.com>
2017-09-27 20:51:05 +0200

    electron-store added to handle settings

--- src/renderer/settings/settings.store.js
+++ src/renderer/settings/settings.store.js
@@ -2,17 +2,27 @@
  * Created by BREMEC Michael <michael@bremec.fr> on 8/23/17
  */
 
-// TODO: make the settings load from config file
-// TODO: use https://github.com/sindresorhus/electron-store
+import ElectronStorage from 'electron-store'
+
 export default {
   state: {
-    pythonPath: '/usr/local/bin/python',
-    generatorPath: '/Users/metikha/Documents/projects/RTC4Water/gpc/watergpc_generic/toolkit/reports/report_generator.py',
-    databasePath: '/Users/metikha/Documents/projects/RTC4Water/gpc/bous/outputs/run29/GPC.db'
+    storage: new ElectronStorage({
+      name: 'report_generator_config',
+      defaults: {
+        pythonPath: '/usr/local/bin/python',
+        generatorPath: '/Users/metikha/Documents/projects/RTC4Water/gpc/watergpc_generic/toolkit/reports/report_generator.py',
+        databasePath: '/Users/metikha/Documents/projects/RTC4Water/gpc/bous/outputs/run29/GPC.db'
+      }
+    })
   },
   getters: {
-    pythonPath: state => state.pythonPath,
-    generatorPath: state => state.generatorPath,
-    databasePath: state => state.databasePath
+    pythonPath: state => state.storage.get('pythonPath'),
+    generatorPath: state => state.storage.get('generatorPath'),
+    databasePath: state => state.storage.get('databasePath')
+  },
+  mutations: {
+    updatePythonPath: (state, payload) => {
+      state.storage.set('pythonPath', payload.pythonPath)
+    }
   }
 }

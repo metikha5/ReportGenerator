d66d3766274fa09513c07dddbbe37ca4fcfae257
Metikha <metikha.sc2@gmail.com>
2017-10-12 13:59:02 +0200

    Log into a folder

--- src/renderer/logging.js
+++ src/renderer/logging.js
@@ -1,13 +1,28 @@
 import { createLogger, format, transports } from 'winston'
-const { combine, timestamp, printf } = format
+import path from 'path'
+import { mkdirSync, existsSync } from 'fs'
 
 let logger
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV !== 'development') {
   logger = {
     info: console.log,
     error: console.error
   }
 } else {
+  const { combine, timestamp, printf } = format
+
+  // Create logs folder
+  let logsFolder = path.join(__dirname, 'logs')
+  if (!existsSync(logsFolder)) {
+    try {
+      mkdirSync(logsFolder)
+    } catch (e) {
+      logsFolder = __dirname
+      console.log(`Unable to create folder: ${logsFolder}`)
+    }
+  }
+
+  //
   const myFormat = printf(info => {
     return `${info.timestamp} --- [${info.level.toUpperCase()}] --- ${info.message}`
   })
@@ -15,9 +30,14 @@ if (process.env.NODE_ENV === 'development') {
     level: 'info',
     format: combine(timestamp(), myFormat),
     transports: [
-      new transports.File({ filename: 'error.log', level: 'error', timestamp: true, handleExceptions: true }),
-      new transports.File({ filename: 'info.log', timestamp: true })
+      new transports.File({ filename: path.join(logsFolder, '/error.log'), level: 'error', timestamp: true }),
+      new transports.File({ filename: path.join(logsFolder, '/info.log'), timestamp: true })
     ]
   })
+
+  if (logsFolder === __dirname) {
+    // We assume it was not possible to create a logs folder
+    logger.error(`Unable to create folder: ${path.join(__dirname, 'logs')}`)
+  }
 }
 export default logger

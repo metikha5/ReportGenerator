2a2cbb9984e51910a6520a3821de6a2f0c069cdb
Metikha <metikha.sc2@gmail.com>
2017-10-18 17:05:23 +0200

    Notification added when the app is started and the database is not valid

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -11,18 +11,20 @@
 <script>
   import MenuView from './menu/menu'
   import { mapGetters } from 'vuex'
+  import { existsSync } from 'fs'
 
   export default {
     name: 'report-generator',
     components: {
       MenuView
     },
-    computed: mapGetters(['arePlotsModified']),
+    computed: mapGetters(['arePlotsModified', 'databasePath']),
     mounted() {
+      // Prevent to quit if current sheet is not saved
       if (process.env.NODE_ENV !== 'development') {
         window.onbeforeunload = (e) => {
           if (this.arePlotsModified) {
-            var answer = confirm('Your current file has not been saved, do you really want to close the application ?')
+            let answer = confirm('Your current file has not been saved, do you really want to close the application ?')
             e.returnValue = answer
 
             if (answer) {
@@ -30,6 +32,20 @@
             }
           }
         }
+      }
+
+      // Init database
+      if (existsSync(this.databasePath)) {
+        this.$store.dispatch('initSuggestions', {databasePath: this.databasePath})
+      } else {
+        this.$notify({
+          type: 'warn',
+          duration: 60000,
+          title: 'Oh no...',
+          text: `No valid database set, we will not be able to help you building your report !
+          <br/>
+          <a href="#/settings">Select a database now</a>`
+        })
       }
     }
   }
